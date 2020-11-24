61956cf12c74409d5fe6957be3ea312ec4a88ace
shawnlinboy <shhenlin@gmail.com>
2020-11-24 15:25:18 +0800

    remove kotlinApp module

--- kotlinApp/src/main/java/com/meizu/mstore/sdk/demo/ActivityExt.kt
+++
@@ -1,6 +0,0 @@
-package com.meizu.mstore.sdk.demo
-
-import android.app.Activity
-import android.widget.Toast
-
-fun Activity.showToast(text: String) = Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
